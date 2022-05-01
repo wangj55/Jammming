28e51b2f8230c9606fe1c7270624f6909ff72e72
Ji Wang <wj42421999@gmail.com>
2022-04-30 22:45:32 -0400

    Update init Track.js

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -16,10 +16,10 @@ class Track extends React.Component {
         return (
             <div className="Track">
                 <div className="Track-information">
-                    {/* <h3><!-- track name will go here --></h3> */}
-                    {/* <p><!-- track artist will go here--> | <!-- track album will go here --></p> */}
+                    <h3>track name</h3>
+                    <p>track artist | track album</p>
                 </div>
-                {/* <button className="Track-action"><!-- + or - will go here --></button> */}
+                {this.renderAction()}
             </div>
         )
     }
