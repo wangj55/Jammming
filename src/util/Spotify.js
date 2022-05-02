ee0a8545ee0f1dec1f0315fcecb2f44637cc013f
Ji Wang <wj42421999@gmail.com>
2022-05-02 15:48:23 -0400

    get Spotify access token and redirect

---
+++ src/util/Spotify.js
@@ -0,0 +1,27 @@
+const clientId = "c48de7a322754b789a6c94d42928e589";
+const redirectUri = "http://localhost:3000/";
+let accessToken;
+
+const Spotify = {
+  getAccessToken() {
+    if (accessToken) {
+      return accessToken;
+    } else {
+      const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+      const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+      if (accessTokenMatch && expiresInMatch) {
+        accessToken = accessTokenMatch[1];
+        const expiresIn = Number(expiresInMatch[1]);
+
+        window.setTimeout(() => (accessToken = ""), expiresIn * 1000);
+        window.history.pushState("Access Token", null, "/");
+      } else {
+        const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+        window.location = accessUrl;
+      }
+    }
+  },
+};
+
+export default Spotify;
