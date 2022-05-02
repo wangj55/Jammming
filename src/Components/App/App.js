2d7de4f103288a527c3e1081886edb302d112729
Ji Wang <wj42421999@gmail.com>
2022-05-02 17:21:22 -0400

    enable saving playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -11,41 +11,9 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      searchResults: [
-        {
-          name: "name1",
-          artist: "artist1",
-          album: "album1",
-          id: 1,
-        },
-        {
-          name: "name2",
-          artist: "artist2",
-          album: "album2",
-          id: 2,
-        },
-        {
-          name: "name3",
-          artist: "artist3",
-          album: "album3",
-          id: 3,
-        },
-      ],
-      playlistName: "playlistName1",
-      playlistTracks: [
-        {
-          name: "playlistName1",
-          artist: "playlistArtist1",
-          album: "playlistAlbum1",
-          id: 4,
-        },
-        {
-          name: "playlistName2",
-          artist: "playlistArtist2",
-          album: "playlistAlbum2",
-          id: 5,
-        },
-      ],
+      searchResults: [],
+      playlistName: "New Playlist",
+      playlistTracks: [],
     };
 
     this.addTrack = this.addTrack.bind(this);
@@ -80,6 +48,9 @@ class App extends React.Component {
 
   savePlaylist() {
     const trackURIs = this.state.playlistTracks.map((track) => track.uri);
+    Spotify.savePlaylist(trackURIs).then(() => {
+      this.setState({ playlistName: "New Playlist", playlistTracks: [] });
+    });
   }
 
   search(term) {
